5bb526b83046d71cf806765067c5abb1622722d2
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-11-15 16:08:18 +0200

    C# Queues

---
+++ Stacks & Queues/ALab08TrafficJam/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrafficJam
+{
+    class Program
+    {
+        static void Main()
+        {
+            //how many cars can pass on green turn
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            int passedCars = 0;
+
+            string command = Console.ReadLine();
+
+            while (command.ToLower()!="end")
+            {
+                if (command.ToLower()!="green")
+                {
+                    //add car to queue
+                    cars.Enqueue(command);
+                }
+
+                if (command.ToLower()=="green")
+                {
+                    // N cars passed throught junction
+                    for (int i = 1; i <= n; i++)
+                    {
+                        //if queue is empty, break, else pass car
+                        if (cars.Count>=1)
+                        {
+                            Console.WriteLine($"{cars.Dequeue()} passed!");
+                            passedCars++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+
+        }
+    }
+}
