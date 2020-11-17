8bc10828e683fd3d9d2294f8c88d7e36e6e8fe8e
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-11-17 15:37:01 +0200

    C# Queue with commands

---
+++ Stacks & Queues/Ex06SongsQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Queue<string> queueSongs = new Queue<string>(input);
+
+            while (queueSongs.Count>0)
+            {
+                string command = Console.ReadLine();
+
+                if (command=="Play")
+                {
+                    queueSongs.Dequeue();
+                }
+
+                if (command.Contains("Add"))
+                {
+                    string commandItems = command.Substring(4, command.Length - 4);
+
+                    if (queueSongs.Contains(commandItems))
+                    {
+                        Console.WriteLine($"{commandItems} is already contained!");
+                    }
+                    else
+                    {
+                        queueSongs.Enqueue(commandItems);
+                    }
+                }
+
+                if (command=="Show")
+                {
+                    Console.WriteLine(String.Join(", ",queueSongs));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
