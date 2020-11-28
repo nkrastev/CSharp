98062d4da4c90ef85012b8dc779d3523f2fd14e5
nkrastev <nikolay.krystev@gmail.com>
2020-11-29 01:30:23 +0200

    C# Advanced, Matrix, Jagged Arrays

---
+++ MultidimensionalArrays/ALab01SumMatrixElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab01SumMatrixElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            var rowsCols = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var rows = rowsCols[0];
+            var cols = rowsCols[1];
+            int[,] intMatrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var currentCol = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < cols; j++)
+                {
+                    intMatrix[i, j] = currentCol[j];
+                }
+            }
+            var totalSum = 0;
+            for (int row = 0; row < intMatrix.GetLength(0); row++)
+            {
+                var colSum = 0;
+                for (int col = 0; col < intMatrix.GetLength(1); col++)
+                {
+                    colSum += intMatrix[row, col];
+                }
+                totalSum += colSum;
+            }
+
+            Console.WriteLine(intMatrix.GetLength(0));
+            Console.WriteLine(intMatrix.GetLength(1));
+            Console.WriteLine(totalSum);
+
+        }
+    }
+}
