07838e96bf50216ca3e34b1afabe9c8465c4f7b6
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-10 14:42:04 +0200

    C# Dictionary

---
+++ SetsAndDictionariesAdvanced/ALab01CountSameValuesinArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ALab01CountSameValuesinArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            var dict = new Dictionary<double, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dict.ContainsKey(input[i]))
+                {
+                    dict[input[i]]++;
+                }
+                else
+                {
+                    dict.Add(input[i], 1);
+                }
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
