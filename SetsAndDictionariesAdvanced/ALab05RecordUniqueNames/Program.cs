843d4302d5ced9b041bd9b9b4f18d72d7e51cac4
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-11 15:10:27 +0200

    C# HashSet

---
+++ SetsAndDictionariesAdvanced/ALab05RecordUniqueNames/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ALab05RecordUniqueNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            HashSet<string> hashSet = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                hashSet.Add(input);
+            }
+            Console.WriteLine(String.Join("\n",hashSet));
+        }
+    }
+}
