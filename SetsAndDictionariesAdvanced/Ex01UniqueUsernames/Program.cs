07b9c555660c66913aa3557167bd4e217ffbca7e
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-12 13:40:15 +0200

    C# HashSet<s>

---
+++ SetsAndDictionariesAdvanced/Ex01UniqueUsernames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex01UniqueUsernames
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var usernames = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+            Console.WriteLine(String.Join("\n",usernames));
+        }
+    }
+}
