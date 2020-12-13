06fe7afc28c83331264e0733e7b45fa38dd2fb0a
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-13 11:19:42 +0200

    C# Hashset, SortedSet

---
+++ SetsAndDictionariesAdvanced/Ex02SetsofElements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex02SetsofElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var firstLenght = numbers[0];
+            var secondLenght = numbers[1];
+            var firstSet = new List<int>();
+            var secondSet = new List<int>();
+            var result = new HashSet<int>();
+
+            for (int i = 0; i < firstLenght; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < secondLenght; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < firstSet.Count; i++)
+            {
+                if (secondSet.Contains(firstSet[i]))
+                {
+                    result.Add(firstSet[i]);
+                }
+            }
+            for (int i = 0; i < secondSet.Count; i++)
+            {
+                if (firstSet.Contains(secondSet[i])) 
+                {
+                    result.Add(secondSet[i]);
+                }
+            }
+            if (result.Count>0)
+            {
+                Console.WriteLine(String.Join(" ",result));
+            }
+        }
+    }
+}
---
+++ SetsAndDictionariesAdvanced/Ex03PeriodicTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex03PeriodicTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = new SortedSet<string>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    result.Add(input[j]);
+                }
+            }
+            Console.WriteLine(String.Join(" ",result));
+        }
+    }
+}
