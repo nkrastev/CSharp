5c0d413ddee02ee552574c593f1c3f62d2697a39
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-14 14:04:26 +0200

    C# Sets

---
+++ SetsAndDictionariesAdvanced/Ex04EvenTimes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex04EvenTimes
+{
+    class Program
+    {
+        static void Main()
+        {
+            var countOfNumbers = int.Parse(Console.ReadLine());
+            var numbers = new Dictionary<int, int>();
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                if (numbers.ContainsKey(input))
+                {
+                    numbers[input]++;
+                }
+                else
+                {
+                    numbers.Add(input, 1);
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                if (item.Value%2==0)
+                {
+                    Console.WriteLine(item.Key);
+                }
+            }
+        }
+    }
+}
