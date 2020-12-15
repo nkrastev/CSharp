b563c6b63f35a6fcc3923f8756793f08b6c571a8
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-15 15:06:22 +0200

    C# Sets & Dictionaries

---
+++ SetsAndDictionariesAdvanced/Ex05CountSymbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex05CountSymbols
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            var result = new SortedDictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (result.ContainsKey(input[i]))
+                {
+                    result[input[i]]++;
+                }
+                else
+                {
+                    result.Add(input[i], 1);
+                }
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
