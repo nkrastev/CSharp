c88d045b118c635edefeac55d1b55682a59e3f31
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-19 13:50:15 +0200

    Recursion, Sum Array Elements

---
+++ Recursion/ALab01RecursiveArraySum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab01RecursiveArraySum
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var index =0;            
+            Console.WriteLine(Sum(arr, index));
+        }
+
+        private static int Sum(int[] arr, int index)
+        {
+            if (index==arr.Length)
+            {
+                return 0;
+            }
+
+            return arr[index]+Sum(arr, index + 1);
+        }
+    }
+}
