bd2a8c90e86e086d1523ec21385ff23ada760d78
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-19 14:01:58 +0200

    Recursion (Factorial)

---
+++ Recursion/ALab02RecursiveFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ALab02RecursiveFactorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        private static int Factorial(int n)
+        {
+            if (n==1)
+            {
+                return 1;
+            }
+            return n * (Factorial(n - 1));
+        }
+    }
+}
