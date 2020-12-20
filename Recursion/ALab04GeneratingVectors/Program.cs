7ce453f250a6aece56963d869d0d19c1e7edb055
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-20 14:20:28 +0200

    Recursion, Vectors

---
+++ Recursion/ALab04GeneratingVectors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ALab04GeneratingVectors
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var arr = new int[n];
+            var index = 0;
+            Generate(arr, index);
+        }
+
+        private static void Generate(int[] vector, int index)
+        {
+            if (index>vector.Length-1)
+            {
+                Console.WriteLine(String.Join("",vector));
+                return;
+            }
+
+            for (int i = 0; i <=1; i++)
+            {
+                vector[index] = i;
+                Generate(vector, index + 1);
+            }
+        }
+    }
+}
