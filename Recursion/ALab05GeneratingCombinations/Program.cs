15d21497e936fa85d767bed46708e759e4ac26be
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-20 14:20:48 +0200

    Combinations

---
+++ Recursion/ALab05GeneratingCombinations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab05GeneratingCombinations
+{
+    class Program
+    {
+        static void Main()
+        {
+            var arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var border = int.Parse(Console.ReadLine());
+            var vector = new int[border];           
+
+            GenerateCombs(arr, vector, 0,0);
+        }
+
+        private static void GenerateCombs(int[] set, int[] vector, int index, int border)
+        {
+            if (index==vector.Length)
+            {
+                Console.WriteLine(String.Join(" ",vector));
+            }
+            else
+            {
+                for (int i = border; i < set.Length; i++)
+                {
+                    vector[index] = set[i];
+                    GenerateCombs(set, vector, index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
