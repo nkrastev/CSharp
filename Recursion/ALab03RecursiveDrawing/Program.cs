57e45b9cd4713fdf3d1a230e8c4860e568315698
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-20 14:20:05 +0200

    Recursive Drawing

---
+++ Recursion/ALab03RecursiveDrawing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ALab03RecursiveDrawing
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Draw(n);
+        }
+
+        private static void Draw(int n)
+        {
+            if (n<=0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*',n));
+            Draw(n - 1);
+            Console.WriteLine(new string('#',n));
+        }
+    }
+}
