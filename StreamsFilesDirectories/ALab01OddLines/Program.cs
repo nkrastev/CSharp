8fe9d754c23d5bc2f993456399184feefee2c34d
nkrastev <nikolay.krystev@gmail.com>
2020-12-21 12:10:11 +0200

    C# Files, Odd Lines

---
+++ StreamsFilesDirectories/ALab01OddLines/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ALab01OddLines
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines("Input.txt");
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i% 2!=0)
+                {
+                    Console.WriteLine(input[i]);
+                }
+            }
+        }
+    }
+}
