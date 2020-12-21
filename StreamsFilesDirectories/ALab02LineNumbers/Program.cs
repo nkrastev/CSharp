a61c7f391381782d0ba030c26d3e3cd35634b911
nkrastev <nikolay.krystev@gmail.com>
2020-12-21 12:11:06 +0200

    C# Files, Line Number

---
+++ StreamsFilesDirectories/ALab02LineNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ALab02LineNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            File.Delete("output.txt");
+            var counter = 1;
+            for (int i = 0; i < input.Length; i++)
+            {                
+                File.AppendAllText("output.txt", $"{counter}. {input[i]}\n");
+                counter++;              
+            }
+        }
+    }
+}
