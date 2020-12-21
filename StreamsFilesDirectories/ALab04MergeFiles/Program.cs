ad8abfb3a98088e576c56f0af52ac99598b5a0c0
nkrastev <nikolay.krystev@gmail.com>
2020-12-21 12:12:02 +0200

    C# Files, Merge

---
+++ StreamsFilesDirectories/ALab04MergeFiles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ALab04MergeFiles
+{
+    class Program
+    {
+        static void Main()
+        {
+            var firstFile = File.ReadAllLines("FileOne.txt");
+            var secondFile = File.ReadAllLines("FileTwo.txt");
+
+            StringBuilder sb = new StringBuilder();
+
+            //we assume the files are with equal lenght
+            for (int i = 0; i < firstFile.Length; i++)
+            {
+                sb.Append(firstFile[i]+"\n");
+                sb.Append(secondFile[i] + "\n");
+            }
+            
+            File.WriteAllText("output.txt", sb.ToString());
+        }
+    }
+}
