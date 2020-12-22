558add12abda2a39b8c90aa944c0db7b97c23d07
nkrastev <nikolay.krystev@gmail.com>
2020-12-22 11:55:10 +0200

    C# Folder Size

---
+++ StreamsFilesDirectories/ALab06FolderSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ALab06FolderSize
+{
+    class Program
+    {
+        static void Main()
+        {
+            var totalSize = 0.0;
+            foreach (string item in Directory.GetFiles("TestFolder"))
+            {             
+                FileInfo infoForFile = new FileInfo(item);
+                totalSize += infoForFile.Length;                
+            }
+            var totalInMB = totalSize / 1024 / 1024;
+            File.WriteAllText("output.txt", totalInMB.ToString());                      
+        }
+    }
+}
