26f7e6c050496aceaeed7ec2187e93b27a2382e2
nkrastev <nikolay.krystev@gmail.com>
2020-12-22 11:55:30 +0200

    C# Even Lines

---
+++ StreamsFilesDirectories/Ex01EvenLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Ex01EvenLines
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("text.txt");
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i %2==0)
+                {
+                    var line = input[i];
+                    //replace
+                    line = Regex.Replace(line, @"[\-\,\.\!\?]", "@");
+                    //reverse
+                    line = String.Join(" ", line.Split(' ').Reverse());
+                    Console.WriteLine(line);
+                }
+            }
+            
+        }
+       
+    }
+}
