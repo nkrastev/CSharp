5fb6fc31b38f52cdadb9fa5ce1477b48a33cf635
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-23 01:08:10 +0200

    C# Copy Binary File

---
+++ StreamsFilesDirectories/Ex04CopyBinaryFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Ex04CopyBinaryFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            Copy("buzludzha.jpg", "copied.jpg");
+        }
+
+        public static void Copy(string inputFilePath, string outputFilePath)
+        {
+            int bufferSize = 1024 * 1024;
+            using (FileStream fileStream = new FileStream(outputFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))            
+            {
+                FileStream fs = new FileStream(inputFilePath, FileMode.Open, FileAccess.ReadWrite);
+                fileStream.SetLength(fs.Length);
+                int bytesRead = -1;
+                byte[] bytes = new byte[bufferSize];
+
+                while ((bytesRead = fs.Read(bytes, 0, bufferSize)) > 0)
+                {
+                    fileStream.Write(bytes, 0, bytesRead);
+                }
+            }
+        }
+    }
+}
