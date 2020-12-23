12e4f6fe6668d48e3cd5b465cc52b32cbd909452
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-24 00:54:50 +0200

    C# Func

---
+++ FunctionalProgramming/ALab02SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab02SumNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)                
+                .ToList();
+
+            Console.WriteLine(input.Count);
+            Console.WriteLine(input.Sum());
+
+        }
+    }
+}
