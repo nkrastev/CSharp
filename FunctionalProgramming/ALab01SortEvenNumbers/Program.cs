80c96e7ab4892783f07049d0b940a22411fedbe5
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-23 15:36:28 +0200

    C# Functional Programming

---
+++ FunctionalProgramming/ALab01SortEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab01SortEvenNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(x=>x%2==0)
+                .OrderBy(x=>x)
+                .ToList();
+            Console.WriteLine(String.Join(", ",input));
+        }
+    }
+}
