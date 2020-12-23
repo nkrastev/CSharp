67e45fef97d5fd39e9ddae3c2fd5f1477981b071
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-24 00:55:14 +0200

    C# Func

---
+++ FunctionalProgramming/ALab03CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab03CountUppercaseWords
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(checker)
+                .ToList()
+                .ForEach(Console.WriteLine);          
+        }
+    }
+}
