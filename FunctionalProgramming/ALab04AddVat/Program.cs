b22569b40f28b9108b407360720aa402fff639e7
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-25 11:14:06 +0200

    C#

---
+++ FunctionalProgramming/ALab04AddVat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ALab04AddVat
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => x * 0.2 + x)
+                .ToList();
+            foreach (var item in input)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
