871ea383604199724b90725b4fd49180d1170844
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-26 15:44:30 +0200

    C# Func, Action

---
+++ FunctionalProgramming/Ex01ActionPrint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex01ActionPrint
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ReadLine()
+                .Split()
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
