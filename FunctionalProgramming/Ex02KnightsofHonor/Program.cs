9334ab11a778cd9e90a210564948b18be4a456ef
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-26 15:45:02 +0200

    C# Functional

---
+++ FunctionalProgramming/Ex02KnightsofHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex02KnightsofHonor
+{
+    class Program
+    {
+        static void Main()
+        {
+            Action<string> print = name => Console.WriteLine($"Sir {name}");
+            
+            var input = Console.ReadLine().Split().ToList();
+            foreach (var item in input)
+            {
+                print(item);
+            }          
+        }
+    }
+}
