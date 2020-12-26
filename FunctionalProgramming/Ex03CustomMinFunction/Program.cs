d9e8d0967a382be0da8b6a5fa826f869549a9ca4
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-26 15:45:26 +0200

    C# Functional

---
+++ FunctionalProgramming/Ex03CustomMinFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex03CustomMinFunction
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .Min(x => x)
+                .ToString();
+            Console.WriteLine(input);
+        }
+    }
+}
