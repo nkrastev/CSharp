bf6410890cdd5abbb0ab15c016697140d30d9430
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-27 13:34:37 +0200

    C# Func

---
+++ FunctionalProgramming/Ex04FindEvensorOdds/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex04FindEvensorOdds
+{
+    class Program
+    {
+        static void Main()
+        {
+            var range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var startRange = range[0];
+            var endRange = range[1];
+
+            var command = Console.ReadLine();
+            Print(startRange, endRange, command);
+            
+        }
+
+        private static void Print(int startRange, int endRange, string command)
+        {
+            if (command=="odd")
+            {
+                for (int i = startRange; i <= endRange; i++)
+                {
+                    if (i % 2!=0)
+                    {
+                        Console.Write(i+" ");
+                    }
+                }
+            }
+            else
+            {
+                for (int i = startRange; i <= endRange; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
