b6af269afb1a4c149e07c051690322a103462671
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-28 07:02:09 +0200

    C# Func

---
+++ FunctionalProgramming/Ex06ReverseAndExclude/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex06ReverseAndExclude
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var divisionBy = int.Parse(Console.ReadLine());
+            var newList = input.Where(x => x % divisionBy != 0).Reverse().ToList();            
+            Console.WriteLine(String.Join(" ",newList));
+        }
+    }
+}
