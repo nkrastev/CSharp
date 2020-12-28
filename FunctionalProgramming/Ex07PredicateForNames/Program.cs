006b7a39740b156ff0518cd862cbab3221902d45
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-28 07:02:30 +0200

    C# Func

---
+++ FunctionalProgramming/Ex07PredicateForNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex07PredicateForNames
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lenght = int.Parse(Console.ReadLine());
+            Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= lenght)
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
