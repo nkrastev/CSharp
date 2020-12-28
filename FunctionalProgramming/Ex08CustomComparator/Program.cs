15426d4a479c5705993af996a2ac1eb4ad4ed2fc
Krastev Nikolay <nkrastev@users.noreply.github.com>
2020-12-28 07:02:46 +0200

    C# Func

---
+++ FunctionalProgramming/Ex08CustomComparator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex08CustomComparator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var odd = input.Where(n => n % 2 == 0).OrderBy(n=>n).ToList();
+            var even = input.Where(n => n % 2 != 0).OrderBy(n=>n).ToList();
+            odd.AddRange(even);
+            Console.WriteLine(String.Join(" ",odd));
+            
+        }
+    }
+}
