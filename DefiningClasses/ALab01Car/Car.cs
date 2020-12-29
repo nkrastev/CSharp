4dbadea3ba4d99af42d4820808ebf5e2b7d8b53d
nkrastev <nikolay.krystev@gmail.com>
2020-12-30 00:11:54 +0200

    C# Classes

---
+++ DefiningClasses/ALab01Car/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        //fields
+        private string make;
+        private string model;
+        private int year;
+
+        //properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+    }
+}
