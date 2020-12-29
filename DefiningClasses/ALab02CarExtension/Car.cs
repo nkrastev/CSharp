494810aed6a011866de6a445b0e7d6c1b0878048
nkrastev <nikolay.krystev@gmail.com>
2020-12-30 00:12:15 +0200

    C# Classes

---
+++ DefiningClasses/ALab02CarExtension/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        //fields
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+
+        //properties
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+        public double FuelQuantity {get;set;}
+        public double FuelConsumption {get;set;}
+
+        public void Drive(double distance)
+        {
+            if (this.FuelQuantity - distance* this.FuelConsumption > 0)
+            {
+                this.FuelQuantity -= this.FuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            string result = $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+            return result;
+        }
+    }
+}
---
+++ DefiningClasses/ALab02CarExtension/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+
+            car.Drive(1);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
