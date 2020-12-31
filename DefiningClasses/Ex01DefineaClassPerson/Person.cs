1d0a6146623c3b66f409e5f636e99e540db83ceb
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-01 00:32:31 +0200

    C# Classes

---
+++ DefiningClasses/Ex01DefineaClassPerson/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    class Person
+    {
+        // fields
+        private string name;
+        private int age;
+
+        // constructor
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }        
+
+        //properties
+        public string Name 
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age 
+        {
+            get { return this.age; }
+            set 
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("The Age cannot be less than 0.");                   
+                }
+                this.age = value; 
+            }
+        }        
+    }
+}
---
+++ DefiningClasses/Ex01DefineaClassPerson/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person first = new Person("Pesho", 0);
+            Person second = new Person("Gosho", 18);
+            Person third = new Person("Stamat", 43);
+        }
+    }
+}
