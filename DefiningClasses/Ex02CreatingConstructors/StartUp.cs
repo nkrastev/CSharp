e6eba10678311474cba33b7d9df3b19ce3180a4f
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-01 00:32:56 +0200

    C# Classes

---
+++ DefiningClasses/Ex02CreatingConstructors/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Person first = new Person();
+            Person second = new Person(5);
+            Person third = new Person("Pesho",7);
+
+            Console.WriteLine(third.Age);
+        }
+    }
+}
