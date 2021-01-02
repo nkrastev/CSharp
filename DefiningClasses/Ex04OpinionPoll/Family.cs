3c352cf78b6cc5484570e86f8cef1f39748186e2
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-02 11:49:32 +0200

    C# Classes

---
+++ DefiningClasses/Ex04OpinionPoll/Family.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        //fields
+        private List<Person> peopleInFamily { get; set; }
+
+        //constructor 
+        public Family()
+        {
+            PeopleInFamily = new List<Person>();
+        }
+
+        //properties
+        public List<Person> PeopleInFamily { get; set; }
+
+        //methods
+        public void AddMember(Person person)
+        {
+            PeopleInFamily.Add(person);
+        }
+        public Person GetOldestMember()
+        {
+            return PeopleInFamily.OrderByDescending(a => a.Age).First();
+        }
+
+        public List<Person> GetOlderThan30()
+        {
+            return PeopleInFamily.Where(x => x.Age > 30).ToList();
+        }
+    }
+}
---
+++ DefiningClasses/Ex04OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var itemName = input[0];
+                var itemAge = int.Parse(input[1]);
+
+                Person person = new Person(itemName, itemAge);
+                family.AddMember(person);
+            }
+
+            List<Person> olderThan30 = family.GetOlderThan30();
+
+            foreach (Person item in olderThan30.OrderBy(x=>x.Name))
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+        }
+    }
+}
