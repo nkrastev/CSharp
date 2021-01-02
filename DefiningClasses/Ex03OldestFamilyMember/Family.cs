ae861b2eae51817a22d83933fe52e4d4ee2ef8d8
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-02 11:49:10 +0200

    C# Classes

---
+++ DefiningClasses/Ex03OldestFamilyMember/Family.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        //fields
+        private List<Person> peopleInFamily { get; set; }
+
+        //constructor 
+        public Family()
+        {
+            PeopleInFamily = new List<Person>();
+        }
+
+        //properties
+        public List<Person> PeopleInFamily { get;set; }
+
+        //methods
+        public void AddMember(Person person)
+        {
+            PeopleInFamily.Add(person);
+        }
+        public Person GetOldestMember()
+        {
+            return PeopleInFamily.OrderByDescending(a => a.Age).First();
+        }
+
+        internal List<Person> ToList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DefiningClasses/Ex03OldestFamilyMember/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var commands = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var itemName = commands[0];
+                var itemAge = int.Parse(commands[1]);
+
+                Person person = new Person(itemName, itemAge);
+                family.AddMember(person);
+            }           
+
+            Console.WriteLine($"{family.GetOldestMember().Name} {family.GetOldestMember().Age}");
+        }
+    }
+}
