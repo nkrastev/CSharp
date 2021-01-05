699d39440c3f4b57e5a03998bbd52f3d09cb8293
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-05 14:07:04 +0200

    C# Classes

---
+++ DefiningClasses/Ex05DateModifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex05DateModifier
+{
+    public class DateModifier
+    {
+        //fields
+        private string dateItem;
+
+        //properties
+        public string DateItem{get;set;}
+
+
+        //methods
+        public static int Calculate(string first, string second)
+        {           
+            DateTime firstDate = DateTime.ParseExact($"{first}", "yyyy MM dd", System.Globalization.CultureInfo.InvariantCulture);
+            DateTime secondDate = DateTime.ParseExact($"{second}", "yyyy MM dd", System.Globalization.CultureInfo.InvariantCulture);
+            var difference = Math.Abs((firstDate.Date - secondDate.Date).Days);
+            return difference;
+        }
+    }
+}
---
+++ DefiningClasses/Ex05DateModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ex05DateModifier
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            Console.WriteLine(DateModifier.Calculate(firstDate, secondDate));            
+        }
+    }
+}
