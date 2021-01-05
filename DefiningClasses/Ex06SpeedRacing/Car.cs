fccadd0ec46c4460960dae5401f581c3b9119dbc
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-05 14:09:03 +0200

    C# Classes

---
+++ DefiningClasses/Ex06SpeedRacing/Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex06SpeedRacing
+{
+    public class Car
+    {
+        //fields
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        //constructor
+        public Car(string model, double fuel, double consumption)
+        {
+            this.Model = model;
+            this.FuelAmount = fuel;
+            this.FuelConsumptionPerKilometer = consumption;
+            this.TravelledDistance = 0;
+        }
+
+        //properties
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+        public double FuelAmount
+        {
+            get { return this.fuelAmount; }
+            set { this.fuelAmount = value; }
+        }
+        public double FuelConsumptionPerKilometer
+        {
+            get { return this.fuelConsumptionPerKilometer; }
+            set { this.fuelConsumptionPerKilometer = value; }
+        }
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Travelled distance cannot be negative.");
+                }
+                this.travelledDistance = value;
+            }
+        }
+
+        //methods
+        public void Drive(double distanceToDrive)
+        {
+            if (((this.FuelAmount - distanceToDrive * this.FuelConsumptionPerKilometer) >= 0.0))
+            {
+                this.FuelAmount -= distanceToDrive * this.FuelConsumptionPerKilometer;
+                this.TravelledDistance += distanceToDrive;
+            }
+            else
+            {
+                //have to throw exception not CW
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            
+        }
+    }
+}
---
+++ DefiningClasses/Ex06SpeedRacing/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex06SpeedRacing
+{
+    class Program
+    {
+        static void Main()
+        {            
+            var listOfCars = ReadCars(int.Parse(Console.ReadLine()));
+            var command = Console.ReadLine();
+
+            while (command!="End")
+            {
+                var cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var carModel = cmdArgs[1];
+                var distance = double.Parse(cmdArgs[2]);
+                
+                //find index of the car to be driven
+                var index = listOfCars.FindIndex(x => x.Model == carModel);
+                
+                //drive the car
+                listOfCars[index].Drive(distance);                
+
+                command = Console.ReadLine();
+            }
+
+            PrintOutput(listOfCars);
+
+
+        }
+
+        private static void PrintOutput(List<Car> listOfCars)
+        {
+            foreach (Car item in listOfCars)
+            {
+                Console.WriteLine($"{item.Model} {item.FuelAmount:f2} {item.TravelledDistance}");
+            }
+        }
+
+        private static List<Car> ReadCars(int numberOfCars)
+        {
+            var list = new List<Car>();
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var model = input[0];
+                var fuel = double.Parse(input[1]);
+                var consumption = double.Parse(input[2]);
+                Car car = new Car(model, fuel, consumption);
+                list.Add(car);
+            }
+            return list;
+        }
+    }
+}
