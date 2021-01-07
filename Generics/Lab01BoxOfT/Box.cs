3d6871e36abeb79dde08efd9a5d69fba528ed637
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-07 14:39:49 +0200

    C# Generics

---
+++ Generics/Lab01BoxOfT/Box.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        //fields
+        private Stack<T> data;        
+
+        //constructor
+        public Box()
+        {
+            this.Data = new Stack<T>();
+        }
+        //properties
+        public Stack<T> Data 
+        {
+            get;
+            set;
+        }
+        public int Count => this.Data.Count;
+
+        //methods
+        public void Add(T element)
+        {
+            this.Data.Push(element);            
+        }
+        public T Remove()
+        {           
+            return this.Data.Pop();
+        }
+    }
+}
---
+++ Generics/Lab01BoxOfT/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Box<string> box = new Box<string>();
+            box.Add("1");
+            box.Add("2");
+            box.Add("3");
+            Console.WriteLine(box.Remove());
+            
+            Console.WriteLine(box.Remove());
+
+            Console.WriteLine(box.Count);
+
+        }
+    }
+}
