ee89e7e5ead09bc552a92a5d9d9ef9debd29fee2
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-08 14:01:13 +0200

    C# Generics

---
+++ Generics/Lab03GenericScale/EqualityScale.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        //fields
+        private T left;
+        private T right;
+
+        //ctor
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        //prop
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        //methods
+        public bool AreEqual()
+        {            
+            if (this.Left.Equals(this.Right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+
+    }
+}
---
+++ Generics/Lab03GenericScale/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var test = new EqualityScale<string>("6", "6");
+            Console.WriteLine(test.AreEqual());
+        }
+    }
+}
