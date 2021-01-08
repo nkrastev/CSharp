44b1ceb570b729baf020ec6d8445f79f7a101d86
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-08 14:00:33 +0200

    C# Generics

---
+++ Generics/Lab02ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        //fields
+
+        //ctor
+
+        //properties
+
+
+
+        //methods
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] arr = new T[length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = item;
+            }
+            return arr;
+        }
+    }
+}
