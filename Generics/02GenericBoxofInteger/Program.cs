07f1850d0944316d64f5c856fc4ade6fdd38036b
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-09 14:38:56 +0200

    C# Generics

---
+++ Generics/02GenericBoxofInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02GenericBoxofInteger
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var numberOfInts = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfInts; i++)
+            {
+                Box<int> box = new Box<int>();
+                box.Item = int.Parse(Console.ReadLine());
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
