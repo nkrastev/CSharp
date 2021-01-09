a3d157f6e2ecd5331cdba4134ccd2c099875185b
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-09 14:38:31 +0200

    C# Generics

---
+++ Generics/01GenericBoxofString/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01GenericBoxofString
+{
+    public class Box<T>
+    {
+        //fields
+        private T item;
+
+        //ctor
+        public Box()
+        {
+            
+        }
+
+        //prop
+        public T Item { get; set; }
+
+        //methods
+        public override string ToString()
+        {
+            return $"{this.Item.GetType()}: {this.Item}";
+        }
+
+    }
+}
---
+++ Generics/01GenericBoxofString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01GenericBoxofString
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var numberOfStrings = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfStrings; i++)
+            {
+                Box<string> box = new Box<string>();
+                box.Item = Console.ReadLine();
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
