542c291c66958d2d9a4124425b0daf7053453c1f
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-12 00:42:54 +0200

    C# Generics

---
+++ Generics/03GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03GenericSwapMethodStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            var customList = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                customList.AddToList(Console.ReadLine());
+            }
+
+            var elementsToSwap = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();            
+            
+            // will not check if indexes are valid
+            customList.Swap(elementsToSwap[0], elementsToSwap[1]);
+
+            Console.WriteLine(customList.Print());
+
+        }
+    }
+}
