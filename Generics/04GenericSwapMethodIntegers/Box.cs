2360a8c1526a42b2d49cbe8dc39a6fc84de4fc16
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-12 00:43:16 +0200

    C# Generics

---
+++ Generics/04GenericSwapMethodIntegers/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04GenericSwapMethodIntegers
+{
+    public class Box<T>
+    {
+        //fields        
+
+        //ctor
+        public Box()
+        {
+            this.Items = new List<T>();
+        }
+
+        //prop
+        public List<T> Items
+        {
+            get;
+            set;
+        }
+
+        //methods
+        public void AddToList(T element)
+        {
+            this.Items.Add(element);
+        }
+        public List<T> Swap(int first, int second)
+        {
+            var temp = this.Items[first];
+            this.Items[first] = this.Items[second];
+            this.Items[second] = temp;
+
+            return this.Items;
+        }
+
+        public string Print()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this.Items.Count; i++)
+            {
+                //$"{this.Item.GetType()}: {this.Item}";
+                sb.AppendLine($"{this.Items[i].GetType()}: {this.Items[i]}");
+            }
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Generics/04GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04GenericSwapMethodIntegers
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            var n = int.Parse(Console.ReadLine());
+            var customList = new Box<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                customList.AddToList(int.Parse(Console.ReadLine()));
+            }
+
+            var elementsToSwap = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            // will not check if indexes are valid
+            customList.Swap(elementsToSwap[0], elementsToSwap[1]);
+
+            Console.WriteLine(customList.Print());
+
+        }
+    }
+}
