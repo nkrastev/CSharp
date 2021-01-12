c0b79ce21c50f6cb40c33c351635b8a9cc8f52c7
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-13 00:43:24 +0200

    C# Generics

---
+++ Generics/06GenericCountMethodDouble/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var customList = new Box<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                customList.AddToList(double.Parse(Console.ReadLine()));
+            }
+
+            //value of the element for comparison
+            var element = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(customList.BiggerElements(element));
+        }
+    }
+}
