7b364c893c90f7a2a18a1f7a4ea94288b3ad16d7
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-13 00:42:56 +0200

    C# Generics

---
+++ Generics/05GenericCountMethodString/Box.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05GenericCountMethodString
+{
+    public class Box<T>
+        where T : IComparable
+    {
+        //fields        
+
+        //ctor
+        public Box()
+        {
+            this.Items = new List<T>();
+        }
+
+        //prop
+        public List<T> Items
+        {
+            get;
+            set;
+        }
+
+        //methods
+        public void AddToList(T element)
+        {
+            this.Items.Add(element);
+        }
+        public List<T> Swap(int first, int second)
+        {
+            var temp = this.Items[first];
+            this.Items[first] = this.Items[second];
+            this.Items[second] = temp;
+
+            return this.Items;
+        }
+
+        public int BiggerElements(T element)
+        {
+            var count = 0;
+
+            for (int i = 0; i < this.Items.Count; i++)
+            {
+                if (this.Items[i].CompareTo(element)>0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+
+    }
+}
---
+++ Generics/05GenericCountMethodString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05GenericCountMethodString
+{
+    class Program
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var customList = new Box<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                customList.AddToList(Console.ReadLine());
+            }
+
+            //value of the element for comparison
+            var element = Console.ReadLine();
+
+            Console.WriteLine(customList.BiggerElements(element));
+        }
+    }
+}
