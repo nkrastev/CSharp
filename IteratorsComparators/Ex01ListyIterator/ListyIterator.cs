981fa8ea74247ad5107e8ff0ac379af8951ec78e
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-16 13:26:53 +0200

    C# Iterators

---
+++ IteratorsComparators/Ex01ListyIterator/ListyIterator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    public class ListyIterator <T>
+    {
+        //fields
+        private int currentIndex;
+        private List<T> items;
+
+
+
+        //ctor
+        public ListyIterator(List<T> initialItems)
+        {
+            this.currentIndex = 0;
+            this.items = initialItems;
+        }
+
+        //prop
+        public int Count => this.items.Count;
+        //public T Current { get; set; }
+
+        //methods
+        public bool HasNext()
+        {
+            if (this.currentIndex<this.Count-1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                this.currentIndex++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Print()
+        {
+            if (this.Count==0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(this.items[this.currentIndex]);
+        }
+    }
+}
---
+++ IteratorsComparators/Ex01ListyIterator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listyIterator = null;                
+
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                try
+                {
+                    var cmdArgs = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    var command = cmdArgs[0];
+  
+                    if (command == "Create")
+                    {
+                        List<string> elements = cmdArgs.Skip(1).ToList();
+                        listyIterator = new ListyIterator<string>(elements);
+                    }
+                    if (command == "HasNext")
+                    {
+                        Console.WriteLine(listyIterator.HasNext());
+                    }
+                    if (command == "Print")
+                    {
+                        listyIterator.Print();
+                    }
+                    if (command == "Move")
+                    {
+                        Console.WriteLine(listyIterator.Move());
+                    }
+                    
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
