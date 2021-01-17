02c425f9cdd393c6c35a2e6b01217a8c023665b4
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-17 15:49:51 +0200

    C# Stack, Iterators

---
+++ IteratorsComparators/Ex03Stack/CustomStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    public class CustomStack<T> :IEnumerable<T>
+    {
+        //fields
+        private List<T> data;
+        
+
+        //ctor
+        public CustomStack()
+        {
+            this.data = new List<T>();
+        }       
+
+        //prop, not in the task
+        public int Count => this.data.Count;
+
+        //methods
+        public void Push(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Pop()
+        {
+            T item;
+            if (this.data.Count>0)
+            {                
+                item = this.data[data.Count - 1];
+                this.data.RemoveAt(data.Count - 1);
+                return item;
+            }
+            else
+            {
+                throw new InvalidOperationException("No elements");
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.data.Count-1; i >= 0; i--)
+            
+                yield return this.data[i];
+        }
+
+        //old interface
+        IEnumerator IEnumerable.GetEnumerator()=> GetEnumerator();        
+    }
+}
---
+++ IteratorsComparators/Ex03Stack/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            CustomStack<string> customStack = new CustomStack<string>();
+            
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                try
+                {
+                    var cmd = input.Split(new Char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (cmd[0]=="Pop")
+                    {
+                        customStack.Pop();
+                    }
+                    if (cmd[0]=="Push")
+                    {                       
+                        List<string> elements = cmd.Skip(1).ToList();
+                        foreach (var item in elements)
+                        {
+                            customStack.Push(item);
+                        }                        
+                    }
+                }
+                catch (InvalidOperationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+       
+    }
+}
