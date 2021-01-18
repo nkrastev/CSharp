b11046006fc9d4a9b6c12db84d566ca4c8011b92
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-18 13:50:11 +0200

    C# Iterators

---
+++ IteratorsComparators/Ex04Froggy/Lake.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Froggy
+{
+    public class Lake :IEnumerable <int>
+    {
+        //fields
+        private List<int> stones;
+
+
+        //ctor
+        public Lake(params int[] stones)
+        {
+            this.stones = stones.ToList();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int count = this.stones.Count;
+            
+            for (int i = 0; i < count; i+=2)
+            {
+                yield return this.stones[i];
+            }
+
+            int lastOdd = count - 1;
+            if (lastOdd % 2==0)
+            {
+                lastOdd -= 1;
+            }
+
+            for (int i = lastOdd; i > 0 ; i-=2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IteratorsComparators/Ex04Froggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1, 2, 3, 4, 5, 6, 7, 8
+            var input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            Lake lake = new Lake(input);
+
+            Console.WriteLine(String.Join(", ", lake));
+
+        }
+    }
+}
