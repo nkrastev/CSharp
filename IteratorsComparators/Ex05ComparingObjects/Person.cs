a33aefbc5caa4d578dfa403cfd0ba8c59bdea976
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-19 14:04:07 +0200

    C# Comparing Objects

---
+++ IteratorsComparators/Ex05ComparingObjects/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        //fields
+        private string name;
+        private int age;
+        private string town;
+
+        //ctor
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        //prop
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        
+
+        public int CompareTo(Person other)
+        {            
+            int resultName = this.Name.CompareTo(other.Name);
+            int resultAge = this.Age.CompareTo(other.Age);
+            int resultTown = this.Town.CompareTo(other.Town);
+
+            if (resultName==0 && resultAge==0 && resultTown==0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
+    }
+}
---
+++ IteratorsComparators/Ex05ComparingObjects/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = ReadPeople();
+
+            var nPerson = int.Parse(Console.ReadLine());          
+            Person searchedPerson = people[nPerson - 1];
+
+            var totalPeople = people.Count;
+            var equalPeople = 0;
+
+            for (int i = 0; i < people.Count; i++)
+            {                               
+                if (searchedPerson.CompareTo(people[i])==0)
+                {                    
+                    equalPeople++;
+                }
+            }
+            if (equalPeople>1)
+            {
+                Console.WriteLine($"{equalPeople} {totalPeople-equalPeople} {totalPeople}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }                        
+        }
+
+        private static List<Person> ReadPeople()
+        {
+            var list = new List<Person>();
+            var command = Console.ReadLine();
+            while (command != "END")
+            {
+                var cmdArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                list.Add(new Person(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]));
+                command = Console.ReadLine();
+            }
+            return list;
+        }
+    }
+}
