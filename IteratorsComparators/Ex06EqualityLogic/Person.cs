16906f76f9404daa6337ae9018ad1eb599dcf71b
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-20 13:49:25 +0200

    C# Iterators

---
+++ IteratorsComparators/Ex06EqualityLogic/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace EqualityLogic
+{
+    public class Person:IComparable<Person>
+    {
+        //fields
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        //prop
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name)!=0)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+            if (this.Age.CompareTo(other.Age) != 0)
+            {
+                return this.Age.CompareTo(other.Age);
+            }
+            return 0;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() + this.Age.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (this.GetHashCode()==obj.GetHashCode())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ IteratorsComparators/Ex06EqualityLogic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            SortedSet<Person> sortedSetPeople = new SortedSet<Person>();
+            HashSet<Person> hashSetPeople = new HashSet<Person>();
+
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                sortedSetPeople.Add(new Person(input[0], int.Parse(input[1])));
+                hashSetPeople.Add(new Person(input[0], int.Parse(input[1])));
+            }
+
+            Console.WriteLine(sortedSetPeople.Count);
+            Console.WriteLine(hashSetPeople.Count);
+
+        }
+
+    }
+}
