37e282e4bbd7eaf93c23b3cb1dfb842f863d3d5a
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-21 17:59:26 +0200

    C# Inheritance

---
+++ Inheritance/Lab02MultipleInheritance/Puppy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Puppy : Dog
+    {
+
+        //methods
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
