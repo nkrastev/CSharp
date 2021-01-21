4d42a2362c012c4a9e7923f506fac20e04c2e8a4
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-21 17:59:48 +0200

    C# Inheritance

---
+++ Inheritance/Lab03HierarchicalInheritance/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+
+        //methods
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+}
---
+++ Inheritance/Lab03HierarchicalInheritance/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Dog : Animal
+    {
+
+        //methods
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+}
