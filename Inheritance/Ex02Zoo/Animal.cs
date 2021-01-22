a3765ec5588f96ad6c5e6575e679ba1194e0c51c
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-22 13:44:22 +0200

    C# Inheritance

---
+++ Inheritance/Ex02Zoo/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        //fields
+        private string name;
+
+        //ctor
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        //prop
+        public string Name { get; set; }
+    }
+}
