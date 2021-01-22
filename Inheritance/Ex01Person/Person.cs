e87ad04c02a7d56fbfc8908cb361f2cdccc64d17
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-22 13:44:59 +0200

    C# Inheritance

---
+++ Inheritance/Ex01Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        //fields
+        private string name;
+        private int age;
+
+        //ctor
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        //prop
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //methods
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Name: {this.Name}, Age: {this.Age}");
+            return sb.ToString().TrimEnd();            
+        }
+
+    }
+}
---
+++ Inheritance/Ex01Person/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Judge 100/100 with no restriction to the age?!
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
+        }
+    }
+}
