d826a6a8bca21fd4d8c3a87623f64064699bff80
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-23 13:47:05 +0200

    C# Inheritance

---
+++ Inheritance/Ex03PlayersAndMonsters/Hero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        //fields
+        private string username;
+        private int level;
+
+        //ctor
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        //prop
+        public string Username { get; set; }
+        public int Level { get; set; }
+
+        //methods
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
---
+++ Inheritance/Ex03PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var bladeKnight = new BladeKnight("test", 3);
+            System.Console.WriteLine(bladeKnight.ToString());
+        }
+    }
+}
