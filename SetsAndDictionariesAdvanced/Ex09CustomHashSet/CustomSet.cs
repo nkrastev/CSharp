2fcb604ee533d88fc30d93223e3ec65ac271c721
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-23 16:39:23 +0200

    C# Custom HashSet
    Hash Function for string

---
+++ SetsAndDictionariesAdvanced/Ex09CustomHashSet/CustomSet.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex09CustomHashSet
+{
+    public class CustomSet
+    {
+        //fields
+        int size = 8;
+        private List<string>[] internalArray;
+
+        //ctor
+        public CustomSet(int capacity = 8)
+        {
+            size = capacity * 8;
+            internalArray = new List<string>[size];
+        }
+
+        //methods
+        public void Add(string element)
+        {
+            int index = HashFunction(element);
+
+            if (internalArray[index]==null)
+            {
+                //create new list on position from hash function and add element in current list
+                internalArray[index] = new List<string>{element};
+            }
+            else
+            {
+                //add element to current list
+                internalArray[index].Add(element);
+            }
+
+        }
+        public bool Contains(string element)
+        {
+            int index = HashFunction(element);
+            if (internalArray[index]!=null)
+            {
+                for (int i = 0; i < internalArray[index].Count; i++)
+                {
+                    if (internalArray[index][i]==element)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private int HashFunction(string element)
+        {
+            //find positions in the array behind the hashset
+            //external hash function, or Math.Abs(GetHashCode())
+
+            return Math.Abs(element.GetHashCode()) % size;
+
+            //int sum = 0;
+            //for (int i = 0; i < element.Length; i++)
+            //{
+            //    sum += element[i];
+            //}
+            //return sum % size;
+
+            //UInt64 hashedValue = 3074457345618258791ul;
+            //for (int i = 0; i < read.Length; i++)
+            //{
+            //    hashedValue += read[i];
+            //    hashedValue *= 3074457345618258799ul;
+            //}
+            //return hashedValue;
+        }
+
+    }
+}
---
+++ SetsAndDictionariesAdvanced/Ex09CustomHashSet/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Ex09CustomHashSet
+{
+    class Program
+    {
+        static void Main()
+        {
+            int size = 50000;
+            CustomSet custom = new CustomSet();
+            List<string> list = new List<string>();
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(i.ToString());
+            }
+            for (int i = 0; i < size; i++)
+            {
+                list.Contains(i.ToString());
+            }
+            watch.Stop();
+            Console.WriteLine($"List time: {watch.Elapsed}");
+
+            
+            watch.Reset();
+            watch.Start();
+            for (int i = 0; i < size; i++)
+            {
+                custom.Add(i.ToString()+i.ToString());
+            }
+            for (int i = 0; i < size; i++)
+            {
+                custom.Contains(i.ToString());
+            }
+            watch.Stop();
+            Console.WriteLine($"Custom hashset time: {watch.Elapsed}");
+
+
+
+        }
+    }
+}
