375ceb3cd63d695f217bef3f7db067d7916a8c77
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-25 14:55:22 +0200

    Inheritance

---
+++ Inheritance/Ex06Animals/Animal.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        //fields
+        private string name;
+        private int age;
+        private string gender;
+        private const string errorMessage = "Invalid input!";
+
+        //ctor       
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        //prop
+        public string Name 
+        {
+            get { return this.name; }
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(errorMessage);
+                }
+                this.name = value;
+            }
+        }
+        public int Age 
+        {
+            get
+            {
+                return this.age;                
+            }
+            private set
+            {                
+                if (value < 0)
+                {
+                    throw new ArgumentException(errorMessage);
+                }
+                this.age=value;                
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            private set
+            {
+                if (value!="Male" && value!="Female")
+                {
+                    throw new ArgumentException(errorMessage);
+                }
+                this.gender = value;
+            } 
+        }
+
+        //methods
+        public abstract string ProduceSound();
+        
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetType().Name}");
+            sb.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            sb.AppendLine($"{this.ProduceSound()}");
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ Inheritance/Ex06Animals/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            const string END_OF_COMMANDS = "Beast!";
+        
+            var command = Console.ReadLine(); //type of animal                       
+            var animalsList = new List<Animal>();
+
+            while (command != END_OF_COMMANDS)
+            {
+                var animalData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries); //animal data
+
+                string name = animalData[0];
+                int age = int.Parse(animalData[1]);
+
+                Animal currentAnimal = null;
+                
+                if (command=="Dog")
+                {
+                    currentAnimal = new Dog(name, age, animalData[2]);
+                }
+                else if (command=="Cat")
+                {
+                    currentAnimal = new Cat(name, age, animalData[2]);
+                }
+                else if (command=="Frog")
+                {
+                    currentAnimal = new Frog(name, age, animalData[2]);
+                }
+                else if (command=="Kitten")
+                {
+                    currentAnimal = new Kitten(name, age);
+                }
+                else if (command=="Tomcat")
+                {
+                    currentAnimal = new Tomcat(name, age);
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+
+                animalsList.Add(currentAnimal);
+                
+
+            }
+            
+            //output
+            foreach (var item in animalsList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
