780c63356266f9eccc2f2f04deeb645538bf5d76
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-26 11:39:32 +0200

    Encapsulation

---
+++ Encapsulation/ALab01Persons/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        //fields       
+
+        public Person(string firstname, string lastname, int age)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Age = age;
+        }
+
+        //ctor
+
+        //prop
+        public string FirstName { get; private set; }
+        public string LastName { get;private set; }
+        public int Age { get; private set; }
+        
+
+        //method
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+    }
+}
