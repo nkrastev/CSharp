f1877e5368849e6bdeff9a14ce9c415e80677d52
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-29 12:31:17 +0200

    Encapsulation

---
+++ Encapsulation/Ex01ClassBoxData/Box.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex01ClassBoxData
+{
+    public class Box
+    {
+        //fields
+        private double length;
+        private double width;
+        private double height;
+
+        //ctor      
+        public Box(double length, double width, double height)
+        {
+            this.Height = height;
+            this.Length = length;
+            this.Width = width;
+        }
+
+        //prop
+        public double Length
+        {
+            get => this.length;
+            private set 
+            {
+                if (value<=0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            } 
+        }
+        public double Width
+        {
+             get => this.width;
+             private set
+             {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        //methods
+        public double SurfaceArea() => (2 * this.Length * this.Width) + (2 * this.Length * this.Height) + (2 * this.Width * this.Height);
+
+        public double Lateral() => (2 * this.Length * this.Height + 2 * this.Width* this.Height);
+
+        public double Volume() => (this.Length * this.Width * this.Height);
+
+
+
+    }
+}
---
+++ Encapsulation/Ex01ClassBoxData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex01ClassBoxData
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var lenght = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+            var errors = string.Empty;
+
+            try
+            {
+                var box = new Box(lenght, width, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");                
+                Console.WriteLine($"Lateral Surface Area - {box.Lateral():f2}");
+                Console.WriteLine($"Volume - {box.Volume():f2}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }                        
+        }
+    }
+}
