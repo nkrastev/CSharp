d66035d2b41c8ac67b5cff5a434f2278a2a8ae39
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-01-29 12:30:43 +0200

    Encapsulation

---
+++ Encapsulation/ALab04Team/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("SoftUni");
+
+            var lines = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < lines; i++)
+            {
+
+                var cmdArgs = Console.ReadLine().Split();
+                try
+                {
+                    var person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+                    team.AddPlayer(person);                    
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }             
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+
+        }
+    }
+}
---
+++ Encapsulation/ALab04Team/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        //fields
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        //ctor
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        //prop
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get => this.firstTeam.AsReadOnly();
+        }
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get => this.reserveTeam.AsReadOnly();
+        }
+
+        public void AddPlayer(Person player)
+        {
+            if (player.Age < 40)
+            {
+                firstTeam.Add(player);
+            }
+            else
+            {
+                reserveTeam.Add(player);
+            }                
+        }
+
+    }
+
+}
