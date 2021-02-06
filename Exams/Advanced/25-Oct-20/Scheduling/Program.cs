fe0107c169447596196166b12f59bde1803b34f6
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-06 14:57:04 +0200

    Exams

---
+++ Exams/Advanced/25-Oct-20/Scheduling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            var inputTasks = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var inputThreads = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var tasks = new Stack<int>(inputTasks);
+            var threads = new Queue<int>(inputThreads);
+
+            var valueOfTaskToKill = int.Parse(Console.ReadLine());
+
+            while (tasks.Count>0 && threads.Count>0)
+            {
+                if (tasks.Peek() == valueOfTaskToKill)
+                {
+                    Console.WriteLine($"Thread with value {threads.Peek()} killed task {tasks.Peek()}");
+                    break;
+                }
+
+                if (threads.Peek()>=tasks.Peek())
+                {
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else if (threads.Peek()<tasks.Peek())
+                {
+                    threads.Dequeue();
+                }                
+            }
+
+            Console.WriteLine(String.Join(" ", threads));
+        }
+    }
+}
