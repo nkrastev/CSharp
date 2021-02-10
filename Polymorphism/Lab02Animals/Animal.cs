e71ca27ed12e3fe0d79cc3b2c84ac64f4f4dbf62
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-10 15:01:31 +0200

    Polymorphism

---
+++ Polymorphism/Lab02Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string food)
+        {
+            this.Name = name;
+            this.FavouriteFood = food;
+        }
+
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
---
+++ Polymorphism/Lab02Animals/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Animal cat = new Cat("Pesho", "Whiskas");
+            Animal dog = new Dog("Gosho", "Meat");
+
+            Console.WriteLine(cat.ExplainSelf());
+            Console.WriteLine(dog.ExplainSelf());
+
+        }
+    }
+}
