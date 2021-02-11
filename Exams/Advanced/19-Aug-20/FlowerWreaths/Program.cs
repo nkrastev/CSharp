eed026cce3b8acca979c7e1a1b74a413fa65da52
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-11 13:55:10 +0200

    Exams

---
+++ Exams/Advanced/19-Aug-20/FlowerWreaths/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlowerWreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var lilies = ReadLilies();
+            var roses = ReadRoses();            
+            var wreathCount = 0;
+            var flowersForLater = 0;
+
+            while (lilies.Count>0 && roses.Count>0)
+            {
+                if (lilies.Peek()+roses.Peek()==15)
+                {
+                    wreathCount++;
+                    lilies.Pop();
+                    roses.Dequeue();
+                }else if (lilies.Peek()+roses.Peek()>15)
+                {
+                    var value = lilies.Pop()-2;
+                    lilies.Push(value);
+                }else if(lilies.Peek() + roses.Peek() < 15)
+                {
+                    flowersForLater += lilies.Pop();
+                    flowersForLater += roses.Dequeue();
+                }
+            }
+
+            var wreathFromLaterFlower = flowersForLater / 15;
+
+            if (wreathCount+wreathFromLaterFlower>=5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreathCount+wreathFromLaterFlower} wreaths!");
+            }
+            else
+            {
+
+                Console.WriteLine($"You didn't make it, you need {5-wreathCount-wreathFromLaterFlower} wreaths more!");
+            }
+        }
+
+        private static Queue<int> ReadRoses()
+        {
+            var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var queue = new Queue<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                queue.Enqueue(input[i]);
+            }
+            return queue;
+        }
+
+        private static Stack<int> ReadLilies()
+        {
+            var input = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var stack = new Stack<int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            return stack;
+        }
+    }
+}
