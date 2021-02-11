fd8e3ef8acc45eb1aa6548bdc296b34afff9df7e
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-11 13:55:27 +0200

    Exams

---
+++ Exams/Advanced/19-Aug-20/VetClinic/Clinic.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        //field
+        private List<Pet> data;
+
+        //ctor
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.data = new List<Pet>();
+        }
+
+        //prop
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+
+        //methods
+        public void Add(Pet pet)
+        {
+            if (data.Count<Capacity)
+            {
+                data.Add(pet);
+            }
+        }
+        public bool Remove(string name)
+        {            
+            Pet pet = data.Find(x => x.Name == name);
+            if (pet != null)
+            {
+                data.Remove(pet);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            //•	Method GetPet(string name, string owner) – returns the pet with the given name and owner or null if no such pet exists.
+            Pet pet = data.Find(x => x.Name == name && x.Owner==owner );
+            if (pet!=null)
+            {
+                return pet;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+            foreach (var item in data)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public Pet GetOldestPet()
+        {
+            //•	Method GetOldestPet() – returns the oldest Pet.
+            Pet pet = data.OrderByDescending(x => x.Age).First();
+            return pet;
+        }
+
+    }
+}
---
+++ Exams/Advanced/19-Aug-20/VetClinic/Pet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+
+        //prop
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+
+        //ctor
+        public Pet(string name, int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
