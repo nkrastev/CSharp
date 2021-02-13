b8681a264428be29b056d4da4315c6350b91bc15
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-13 16:18:07 +0200

    Exams

---
+++ Exams/Advanced/24-Feb-19/Heroes/HeroRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Heroes
+{
+    public class HeroRepository
+    {
+        private List<Hero> data;
+
+        public HeroRepository()
+        {
+            this.data = new List<Hero>();
+        }
+
+        public int Count { get => this.data.Count; }
+
+        public void Add(Hero hero)
+        {
+            this.data.Add(hero);
+        }
+
+        //•	Method Remove(string name) – removes an entity by given hero name.
+        public void Remove(string name)
+        {
+            int index = data.FindIndex(x => x.Name == name);
+            if (index >= 0)
+            {
+                data.RemoveAt(index);                
+            }
+        }
+        
+        public Hero GetHeroWithHighestStrength() => data.OrderByDescending(x => x.Item.Strength).FirstOrDefault();
+        public Hero GetHeroWithHighestAbility() => data.OrderByDescending(x => x.Item.Ability).FirstOrDefault();
+        public Hero GetHeroWithHighestIntelligence() => data.OrderByDescending(x => x.Item.Intelligence).FirstOrDefault();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Оverride ToString() – Print all the heroes.
+            foreach (var h in data)
+            {
+                sb.AppendLine($"Hero: {h.Name} - {h.Level}lvl");
+                sb.AppendLine($"Item:");
+                sb.AppendLine($"    * Strength: {h.Item.Strength}");
+                sb.AppendLine($"    * Ability: {h.Item.Ability}");
+                sb.AppendLine($"    * Intelligence: {h.Item.Intelligence}");
+            }            
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ Exams/Advanced/24-Feb-19/Heroes/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes
+{
+    public class Item
+    {
+        public Item(int strenght, int ability, int intelligence)
+        {
+            this.Strength = strenght;
+            this.Ability = ability;
+            this.Intelligence = intelligence;
+        }
+        public int Strength { get; set; }
+        public int Ability { get; set; }
+        public int Intelligence { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Item:");
+            sb.AppendLine($"  * Strength: {this.Strength}");
+            sb.AppendLine($"  * Ability: {this.Ability}");
+            sb.AppendLine($"  * Intelligence: {this.Intelligence}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
