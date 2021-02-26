44b7b3a18656fa49c2b7f5839b8f5e7e033442fd
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-26 14:57:16 +0200

    Abstraction

---
+++ Abstraction/Ex01DefineInterfaceIPerson/Citizen.cs
@@ -0,0 +1,16 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get => this.name; set => this.name = value; }
+        public int Age { get => this.age; set => this.age=value; }
+    }
+}
