4126124424f9c1ed0f8a233b6cdaca525b5ab1a4
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-02-27 14:24:20 +0200

    Abstraction

---
+++ Abstraction/Ex02MultipleImplementation/Citizen.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+        private string name;
+        private int age;
+        private string id;
+        private string birthdate;
+
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthday;
+        }
+        public string Name { get => this.name; set => this.name = value; }
+        public int Age { get => this.age; set => this.age=value; }
+        public string Birthdate { get => this.birthdate; set => this.birthdate = value; }
+        public string Id { get => this.id; set => this.id = value; }
+    }
+}
