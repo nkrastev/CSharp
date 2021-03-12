62efdf1f6fefef0fdada6c215c716895a576b16c
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-03-12 13:33:47 +0200

    Reflection

---
+++ Reflection/Lab05CreateAttribute/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ventsi")]
+    public class StartUp
+    {
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
