9929c91c3dc592ef6186575af81a8b6fd6f0abba
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-03-13 09:05:37 +0200

    Reflection

---
+++ Reflection/Lab06CodeTracker/Tracker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public BindingFlags BlindFlag { get; private set; }
+
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+           
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var item in methods)
+            {
+                if (item.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = item.GetCustomAttributes(false);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{item.Name} {attr.Name}");
+                    }
+                }
+            }
+
+        }
+    }
+}
