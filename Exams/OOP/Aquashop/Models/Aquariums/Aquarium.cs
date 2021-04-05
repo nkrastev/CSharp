a05c1e24e998514d6ae074d5779f8c92e4aecce0
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-04-05 14:28:17 +0300

    Aquashop Structure

---
+++ Exams/OOP/Aquashop/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,94 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        private List<IFish> fish;
+        private List<IDecoration> decorations;
+
+        public Aquarium(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.fish = new List<IFish>();
+            this.decorations = new List<IDecoration>();
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAquariumName);
+                }
+                this.name = value;
+            }
+        }
+
+        public int Capacity
+        {
+            get;
+            private set;
+        }
+        public int Comfort => this.decorations.Sum(x => x.Comfort);
+
+        public ICollection<IDecoration> Decorations => this.decorations.AsReadOnly();
+
+        public ICollection<IFish> Fish => this.fish.AsReadOnly();
+
+        public void AddDecoration(IDecoration decoration)
+        {
+            this.decorations.Add(decoration);
+        }
+
+        public void AddFish(IFish fish)
+        {
+            if (this.Capacity==this.fish.Count)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughCapacity);
+            }
+            this.fish.Add(fish);
+        }
+
+        public void Feed()
+        {
+            foreach (var item in fish)
+            {
+                item.Eat();
+            }
+        }
+
+        public string GetInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Name} ({this.GetType().Name})");
+            var fishNames = string.Empty;
+            foreach (var item in fish)
+            {
+                fishNames += item.Name+", ";
+            }
+            fishNames.TrimEnd();
+            if (fishNames==string.Empty)
+            {
+                fishNames = "none";
+            }
+            sb.AppendLine($"Fish {fishNames}");
+            sb.AppendLine($"Decorations: {decorations.Count}");
+            sb.AppendLine($"Comfort: {this.Comfort}");
+            return sb.ToString().TrimEnd();
+        }
+
+        public bool RemoveFish(IFish fish) => this.fish.Remove(fish);
+       
+    }
+}
---
+++ Exams/OOP/Aquashop/Repositories/DecorationRepository.cs
@@ -0,0 +1,27 @@
+﻿using AquaShop.Models.Decorations;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    public class DecorationRepository : IRepository<Decoration>
+    {
+        private List<Decoration> models;
+        public DecorationRepository()
+        {
+            this.models = new List<Decoration>();
+        }
+
+        public IReadOnlyCollection<Decoration> Models => this.models.AsReadOnly();
+
+        public void Add(Decoration model) => this.models.Add(model);
+
+        public Decoration FindByType(string type) => this.models.FirstOrDefault(x => x.GetType().Name == type);      
+
+        public bool Remove(Decoration model) => this.models.Remove(model);
+    }
+}
