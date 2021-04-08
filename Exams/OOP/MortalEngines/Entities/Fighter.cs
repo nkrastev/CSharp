fd27601c5278576ff19b82ed08425a4d20696d0a
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-04-08 15:03:23 +0300

    Fighter Machine Added

---
+++ Exams/OOP/MortalEngines/Entities/Fighter.cs
@@ -0,0 +1,44 @@
+﻿using MortalEngines.Entities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MortalEngines.Entities
+{
+    public class Fighter : BaseMachine, IFighter
+    {
+        private const double INIT_HEALTHPOINTS = 200;
+        public Fighter(string name, double attackPoints, double defensePoints) 
+            : base(name, attackPoints, defensePoints, INIT_HEALTHPOINTS)
+        {
+            this.AggressiveMode = true;
+        }
+        public bool AggressiveMode
+        {
+            get;
+            private set;
+        }
+
+        public void ToggleAggressiveMode()
+        {           
+            if (this.AggressiveMode==true)
+            {
+                this.AggressiveMode = false;
+                this.AttackPoints += 50;
+                this.DefensePoints -= 25;
+            }
+            else
+            {
+                this.AggressiveMode = true;
+                this.AttackPoints -= 50;
+                this.DefensePoints += 25;
+            }
+        }
+
+        public override string ToString()
+        {
+            var mode = (this.AggressiveMode) ? "ON" : "OFF";
+            return base.ToString()+Environment.NewLine+$" *Aggressive: {mode}";
+        }
+    }
+}
