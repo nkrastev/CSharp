1cecbbc028af631ce4e52dd1b1b39077c9cd0154
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-04-08 14:40:05 +0300

    BaseMachine added

---
+++ Exams/OOP/MortalEngines/Entities/BaseMachine.cs
@@ -0,0 +1,95 @@
+﻿using MortalEngines.Common;
+using MortalEngines.Entities.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MortalEngines.Entities
+{
+    public abstract class BaseMachine : IMachine
+    {
+        private string name;
+        private IPilot pilot;
+
+        public BaseMachine(string name, double attackPoints, double defensePoints, double healthPoints)
+        {
+            this.Name = name;
+            this.AttackPoints = attackPoints;
+            this.DefensePoints = defensePoints;
+            this.HealthPoints = healthPoints;
+            this.Pilot = new Pilot();
+            this.Targets = new List<String>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Machine name cannot be null or empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public IPilot Pilot
+        {
+            get => pilot;
+            set
+            {
+                if (value==null)
+                {
+                    throw new NullReferenceException("Pilot cannot be null.");
+                }
+                this.pilot = value;
+            }
+        }
+        public double HealthPoints { get; set; }
+
+        public double AttackPoints { get; private set; }
+
+        public double DefensePoints { get; private set; }
+
+        public IList<string> Targets { get; private set; }
+
+        public void Attack(IMachine target)
+        {          
+            if (target==null)
+            {
+                throw new NullReferenceException("Target cannot be null");
+            }
+           
+            target.HealthPoints = this.AttackPoints - target.DefensePoints;
+            
+            if (target.HealthPoints<0)
+            {
+                target.HealthPoints = 0;
+            }
+
+            this.Targets.Add(target.Name);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"- {this.Name}");
+            sb.AppendLine($" *Type: {this.GetType().Name}");
+            sb.AppendLine($" *Health: {this.HealthPoints}");
+            sb.AppendLine($" *Attack: {this.AttackPoints}");
+            sb.AppendLine($" *Defense: {this.DefensePoints}");
+
+            if (this.Targets.Count==0)
+            {
+                sb.AppendLine($" *Targets: None");
+            }
+            else
+            {
+                sb.AppendLine($" *Targets: {String.Join(",",this.Targets)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
