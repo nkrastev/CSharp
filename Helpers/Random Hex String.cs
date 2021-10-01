e6585b7a4157f52d857b481b50f2e3987a0c43bc
Nikolay Krastev <nkrastev@users.noreply.github.com>
2021-10-01 22:10:45 +0300

    BitConverter

--- Helpers/Random Hex String.cs
+++ Helpers/Random Hex String.cs
@@ -1,22 +1,11 @@
-private static string RandomHexString(int size)
-        {
-            StringBuilder builder = new StringBuilder();
-            Random random = new Random();
-            char ch;
-            for (int i = 0; i < size; i++)
-            {
-                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
-                builder.Append(ch);
-            }
-            //String is ready, convert to to HEX
-            string randomString=builder.ToString();
+private static string RandomByteHexString(int size)
+{            
+    //random bytes generation
+    Random rnd = new Random();
+    Byte[] randomBytesArray = new Byte[size];
+    rnd.NextBytes(randomBytesArray);
 
-            var resultHex = new StringBuilder();
-
-            var bytes = Encoding.Unicode.GetBytes(randomString);
-            foreach (var t in bytes)
-            {
-                resultHex.Append(t.ToString("X2"));
-            }
-            return resultHex.ToString(); // returns: "48656C6C6F20776F726C64" for "Hello world"
-        }  
+    //convertion to hex
+    string resultHex = BitConverter.ToString(randomBytesArray).Replace("-", "");                        
+    return resultHex.ToString();
+}
