d4f2d4b0bd752cde01b85509d530f86fd6b11444
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-11-07 14:58:04 +0200

    HTTP Response

---
+++ Web/HTTP Protocol/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string URL = "https://www.softuni.bg/";
+
+            await ReadData(URL);
+        }
+
+        public static async Task ReadData(string url)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //Bypass the certificate, open connection, get response
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            HttpClient client = new HttpClient(clientHandler);
+
+            var response = await client.GetAsync(url);
+
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(string.Join(Environment.NewLine,
+                response.Headers.Select(x => x.Key + ": " + x.Value.FirstOrDefault())));
+        }
+    }
+}
