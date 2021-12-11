0b66054f3d949fb178f5136416a0951ef4ada4cb
Krastev Nikolay <nkrastev@users.noreply.github.com>
2021-12-11 16:19:07 +0200

    GET, POST

---
+++ Web/HTTP Protocol/WebCats/Controllers/CatsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace WebCats.Controllers
+{
+    public class CatsController : Controller
+    {
+        // Cats/method name the URL
+        // return view returns html
+        public IActionResult SendCatData()
+        {
+            return View();
+        }
+
+        public IActionResult ViewSubmittedData()
+        {
+            //TODO read the submitted data
+
+            return View();
+        }
+    }
+}
---
+++ Web/HTTP Protocol/WebCats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebCats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
