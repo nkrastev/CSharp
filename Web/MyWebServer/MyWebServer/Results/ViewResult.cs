d57b2da069864b792c699a469abd38466943aba9
nkrastev <nikolay.krystev@gmail.com>
2021-12-28 19:44:52 +0200

    Added Sessions & Error handling

---
+++ Web/MyWebServer/MyWebServer/Results/ViewResult.cs
@@ -0,0 +1,76 @@
+﻿namespace MyWebServer.Results
+{
+    using MyWebServer.Http;
+    using System.IO;
+    using System.Linq;
+
+    public class ViewResult : ActionResult
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResult(
+            HttpResponse response,
+            string viewName, 
+            string controllerName, 
+            object model)
+            : base(response) 
+            => this.GetHtml(viewName, controllerName, model);
+
+        private void GetHtml(string viewName, string controllerName, object model)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = controllerName + PathSeparator + viewName;
+            }
+
+            var viewPath = Path.GetFullPath($"./Views/" + viewName.TrimStart(PathSeparator) + ".cshtml");
+
+            if (!File.Exists(viewPath))
+            {
+                this.PrepareMissingViewError(viewPath);
+
+                return;
+            }
+
+            var viewContent = File.ReadAllText(viewPath);
+
+            if (model != null)
+            {
+                viewContent = this.PopulateModel(viewContent, model);
+            }
+
+            this.PrepareContent(viewContent, HttpContentType.Html);
+        }
+
+        private void PrepareMissingViewError(string viewPath)
+        {
+            this.StatusCode = HttpStatusCode.NotFound;
+
+            var errorMessage = $"View '{viewPath}' was not found.";
+
+            this.PrepareContent(errorMessage, HttpContentType.PlainText);
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                .GetType()
+                .GetProperties()
+                .Select(pr => new
+                {
+                    pr.Name,
+                    Value = pr.GetValue(model)
+                });
+
+            foreach (var entry in data)
+            {
+                const string openingBrackets = "{{";
+                const string closingBrackets = "}}";
+
+                viewContent = viewContent.Replace($"{openingBrackets}{entry.Name}{closingBrackets}", entry.Value.ToString());
+            }
+
+            return viewContent;
+        }
+    }
+}
