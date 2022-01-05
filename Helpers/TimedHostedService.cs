813b48a9bb29b820a7bdf1128b4ba717dcc9e2e7
Nikolay Krastev <nkrastev@users.noreply.github.com>
2022-01-05 19:17:55 +0200

    The equivalent of Cron Job in Unix

---
+++ Helpers/TimedHostedService.cs
@@ -0,0 +1,53 @@
+﻿namespace AppForTests.Helpers
+{    
+    using AppForTests.Data.Models;    
+
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private int executionCount = 0;
+        private readonly ILogger<TimedHostedService> _logger;
+        private Timer _timer = null!;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object? state)
+        {           
+            var count = Interlocked.Increment(ref executionCount);
+            
+            var context = new AppForTestsDbContext();
+            var sharoItem = context.Cats.Where(x => x.Name == "Sharo").FirstOrDefault();
+            sharoItem.NumberOfLegs += 1;
+
+            context.SaveChanges();
+
+            _logger.LogInformation(
+                "Timed Hosted Service is working. Count: {Count}", count);
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
