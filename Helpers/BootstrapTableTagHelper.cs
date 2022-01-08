0c18b2af5d70de31a7e66730ef6ae4172858c7ab
Nikolay Krastev <nkrastev@users.noreply.github.com>
2022-01-08 17:59:22 +0200

    TagHelper

---
+++ Helpers/BootstrapTableTagHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace AppForTests.Helpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("table", Attributes = "bootstrap-table")]
+    public class BootstrapTableTagHelper : TagHelper
+    {        
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.Add(new TagHelperAttribute("class", "table table-stripped table-hover table-sm"));
+        }
+    }
+}
