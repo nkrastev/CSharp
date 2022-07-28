169be1807067f841cd2ab943dc715a8694fb48ad
Nikolay Krastev <nkrastev@users.noreply.github.com>
2022-07-28 19:08:34 +0300

    Update SetModalToOpen.cs

--- Helpers/SetModalToOpen.cs
+++ Helpers/SetModalToOpen.cs
@@ -1,13 +1,7 @@
-/*
-<script type="text/javascript">
-    function openModal() {
-        $('#myModal').modal('show');
-    }
-</script>
-*/
+
 
 // Bypass AutoPostBack="true" from asp:Listbox onChange
 
-protected void lbEdit_Click(object sender, EventArgs e) {   
-      ScriptManager.RegisterStartupScript(this,this.GetType(),"Pop", "openModal();", true);
+protected void lbEdit_Click(object sender, EventArgs e) {       
+    ClientScript.RegisterStartupScript(this.GetType(), "Popup", "let modal = new bootstrap.Modal(suppliersModal);modal.show();", true);
 }
