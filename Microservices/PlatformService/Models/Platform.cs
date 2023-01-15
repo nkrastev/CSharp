6b39295f51529e81ff5e4c49a91dc1e44edbd6d1
Nikolay Krastev <nikolay.krystev@gmail.com>
2023-01-15 17:14:22 +0200

    Initial setup

---
+++ Microservices/PlatformService/Models/Platform.cs
@@ -0,0 +1,13 @@
+﻿namespace PlatformService.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Cost { get; set; }
+    }
+}
